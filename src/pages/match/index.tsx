73054952a943f8754fcad6c712575d0af4d4127f
kapila1234 <Kapila.duple@gmail.com>
2024-02-09 11:19:52 +0530

    matchlist update on declare

--- src/pages/match/index.tsx
+++ src/pages/match/index.tsx
@@ -4,12 +4,26 @@ import Match from "../../components/MatchDetail/MatchOdds/Match";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../store/store";
 import { getMatchList } from "../../store/actions/match/matchListAction";
+import { expertSocketService, socketService } from "../../socketManager";
 
 const Matches = () => {
   const dispatch: AppDispatch = useDispatch();
 
+  const getMatchListService = () => {
+    dispatch(getMatchList({}));
+  };
+
   useEffect(() => {
     dispatch(getMatchList({}));
+  }, []);
+
+  useEffect(() => {
+    try {
+      expertSocketService.match.matchAdded(getMatchListService);
+      socketService.userBalance.matchResultDeclared(getMatchListService);
+    } catch (e) {
+      console.log(e);
+    }
   }, []);
 
   return (
