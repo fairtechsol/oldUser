a63488f60fe3d959ed3e6576491bbedc03a9ab95
dhanesh241 <dhanesh.duple@gmail.com>
2024-02-15 10:05:57 +0530

    lazyloading error handling

--- src/utils/loadable.tsx
+++ src/utils/loadable.tsx
@@ -1,9 +1,21 @@
-import { Suspense } from "react";
+import React, { lazy, Suspense } from "react";
 import Loader from "../components/Loader";
-const Loadable = (Component: any) => (props: any) =>
-  (
+
+const Loadable = (importFunc: any) => {
+  const LazyComponent = lazy(() =>
+    importFunc().catch((error: any) => {
+      console.error("Dynamic import failed:", error);
+      // Handle the error, e.g., show an error message
+      window.location.reload();
+      throw error;
+    })
+  );
+
+  return (props: any) => (
     <Suspense fallback={<Loader height={"100vh"} />}>
-      <Component {...props} />
+      <LazyComponent {...props} />
     </Suspense>
   );
+};
+
 export default Loadable;
