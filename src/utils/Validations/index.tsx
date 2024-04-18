2004cb4bdb86f2105ac50164d254df7eb14d0dd5
PardeepDuple <pradeep.duple@gmail.com>
2024-04-18 16:21:29 +0530

    validation file and constants

--- src/utils/Validations/index.tsx
+++ src/utils/Validations/index.tsx
@@ -17,18 +17,58 @@ export const depositAmountValidations = Yup.object({
 export const newPasswordValidationSchema = Yup.object({
   oldPassword: Yup.string().required("Password is required"),
   newPassword: Yup.string()
-  .required("New password is required")
-  .matches(
-    /^(?=.*[A-Z])/, "Password must contain at least one uppercase letter"
-  )
-  .matches(
-    /^(?=.*[a-zA-Z].*[a-zA-Z].*[a-zA-Z].*[a-zA-Z])/, "Password must contain at least four alphabet letters"
-  )
-  .matches(
-    /^(?=.*\d.*\d.*\d.*\d)/, "Password must contain at least four numbers"
-  ),
-  // .notOneOf([Yup.ref('oldPassword'), null], 'New password must be different from old password'),
-confirmPassword: Yup.string()
-  .oneOf([Yup.ref("newPassword"), ""], "Passwords must match")
-  .required("Confirm password is required"),
+    .required("New password is required")
+    .matches(
+      /^(?=.*[A-Z])/,
+      "Password must contain at least one uppercase letter"
+    )
+    .matches(
+      /^(?=.*[a-zA-Z].*[a-zA-Z].*[a-zA-Z].*[a-zA-Z])/,
+      "Password must contain at least four alphabet letters"
+    )
+    .matches(
+      /^(?=.*\d.*\d.*\d.*\d)/,
+      "Password must contain at least four numbers"
+    ),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("newPassword"), ""], "Passwords must match")
+    .required("Confirm password is required"),
 });
+
+export const changePasswordValidation = (item: any) => {
+  return Yup.object({
+    oldPassword: Yup.string()
+      .required("Old Password is required")
+      .test({
+        name: "oldPassword",
+        message: "Old Password Does Not Match",
+        test: async function (value: any) {
+          if (value) {
+            try {
+              return item;
+            } catch (error: any) {
+              console.log(error);
+            }
+          }
+          return true;
+        },
+      }),
+    newPassword: Yup.string()
+      .required("New password is required")
+      .matches(
+        /^(?=.*[A-Z])/,
+        "Password must contain at least one uppercase letter"
+      )
+      .matches(
+        /^(?=.*[a-zA-Z].*[a-zA-Z].*[a-zA-Z].*[a-zA-Z])/,
+        "Password must contain at least four alphabet letters"
+      )
+      .matches(
+        /^(?=.*\d.*\d.*\d.*\d)/,
+        "Password must contain at least four numbers"
+      ),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("newPassword"), ""], "Passwords must match")
+      .required("Confirm password is required"),
+  });
+};
