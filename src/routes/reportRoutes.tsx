17bc685a02477acdba8cc366130d221ebd22e4a5
PardeepDuple <pradeep.duple@gmail.com>
2024-04-23 16:00:55 +0530

    removed lazy loading for protecting build errors

--- src/routes/reportRoutes.tsx
+++ src/routes/reportRoutes.tsx
@@ -1,15 +1,18 @@
-import Loadable from "../utils/loadable";
+// import Loadable from "../utils/loadable";
 import { Navigate } from "react-router-dom";
 import { Constants } from "../utils/Constants";
 
 import MainLayout from "../layout/main";
-import { lazy } from "react";
+// import { lazy } from "react";
+import ProfitLoss from "../pages/reports/ProfitLoss";
+import BetHistory from "../pages/betHistory";
+import AccountStatement from "../pages/reports/AccountStatement";
 
-const ProfitLoss = Loadable(lazy(() => import("../pages/reports/ProfitLoss")));
-const BetHistory = Loadable(lazy(() => import("../pages/betHistory")));
-const AccountStatement = Loadable(
-  lazy(() => import("../pages/reports/AccountStatement"))
-);
+// const ProfitLoss = Loadable(lazy(() => import("../pages/reports/ProfitLoss")));
+// const BetHistory = Loadable(lazy(() => import("../pages/betHistory")));
+// const AccountStatement = Loadable(
+//   lazy(() => import("../pages/reports/AccountStatement"))
+// );
 
 const ReportRoutes = {
   path: Constants.ReportsPaths.root,
