36f489477dc0a7efc6a6049d622134286fde6a5b
PardeepDuple <pradeep.duple@gmail.com>
2024-06-11 17:00:50 +0530

    endpoint for dev

--- src/pages/matchDetail/index.tsx
+++ src/pages/matchDetail/index.tsx
@@ -328,8 +328,8 @@ const MatchDetail = () => {
   const getScoreBoard = async (marketId: string) => {
     try {
       const response: any = await service.get(
-        // `https://devscore.fairgame.club/score/getMatchScore/${marketId}`
-        `https://scoreboard.fairgame7.com/score/getMatchScore/${marketId}`
+        `https://devscore.fairgame.club/score/getMatchScore/${marketId}`
+        // `https://scoreboard.fairgame7.com/score/getMatchScore/${marketId}`
       );
       if (response) {
         setLiveScoreBoardData(response);
