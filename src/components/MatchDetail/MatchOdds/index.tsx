266fe2348592caed90f850132cadc5b41ed61143
Pardeepfairtech <pardeepfairtech@gmail.com>
2025-03-31 13:14:00 +0530

    fix: reduce interval for fetching match market data from 60s to 3s

--- src/components/MatchDetail/MatchOdds/index.tsx
+++ src/components/MatchDetail/MatchOdds/index.tsx
@@ -150,7 +150,7 @@ const MatchesComponent = () => {
       if (type) {
         getMatchListMarket(type || "");
       }
-    }, 60000);
+    }, 3000);
 
     return () => clearInterval(intervalId);
   }, [type]);
