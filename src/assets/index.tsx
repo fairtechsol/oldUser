427c6e0fe0ed8e9c5ca922727531cdc2703f628a
Pardeepfairtech <pardeepfairtech@gmail.com>
2025-04-18 16:14:49 +0530

    Remove unused image assets and update index.tsx exports accordingly

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,5 +1,4 @@
 export { default as ARROWUP } from "./images/arrowup1.webp";
-export { default as BETPLACED } from "./images/betplaced.png";
 export { default as CancelDark } from "./images/cancel.webp";
 export { default as DELETE } from "./images/delete.webp";
 export { default as DownIcon } from "./images/down.svg";
@@ -12,21 +11,15 @@ export { default as Lock } from "./images/lock.webp";
 export { default as FgLogo, default as logo } from "./images/logo02.webp";
 export { default as Logout } from "./images/logout.webp";
 export { default as mail } from "./images/mail.png";
-export {
-    default as loginBackground,
-    default as LoginBg
-} from "./images/new_back.webp";
-export { default as NOT } from "./images/not.webp";
+export { default as LoginBg } from "./images/new_back.webp";
 export { default as Search } from "./images/search.webp";
 export { default as SEARCH } from "./images/search1.webp";
-export { default as TIME } from "./images/time.webp";
 
 export { default as ArrowDown } from "./images/arrow_down.webp";
 export { default as BallStart } from "./images/ballStart.webp";
 
 export { default as Cricket } from "./images/cricket.webp";
 export { default as Football } from "./images/football.webp";
-export { default as Slot } from "./images/slot.webp";
 
 export { default as Card } from "./images/card.webp";
 export { default as Hockey } from "./images/hockey.webp";
@@ -43,16 +36,14 @@ export { default as SNOOKER } from "./images/snooker.webp";
 
 export { default as ARROWDROPDOWN } from "./images/arrowDropDown.webp";
 
-export { default as HourGlass } from "./hourglass.json";
 export { default as ARROW_DOWN } from "./images/arrowDown.webp";
 export { default as ARROW_UP } from "./images/arrowUp.webp";
 export { default as liveTv } from "./images/live_tv.svg";
 export { default as LockIcon } from "./images/lock.svg";
 export { default as UD } from "./images/UD.webp";
 
-export { default as ColorPrediction } from "./images/colorPrediction.png";
+export { default as ColorPrediction } from "./images/colorPrediction.png"; //
 export { default as dt2020 } from "./images/dt202.webp";
-export { default as MacExcite } from "./images/macExcite.png";
 export { default as MacVirtual } from "./images/macVirtual.png";
 
 export { default as Thirty2CardsA } from "./images/32Cards.webp";
