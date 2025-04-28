38e63e5644a566441adbed98af4d6fdc116e2d35
Pardeepfairtech <pardeepfairtech@gmail.com>
2025-04-28 10:19:02 +0530

    fix: correct arrow rotation logic based on visibility state

--- src/components/MatchDetail/CricketCasinoOdds/CricketCasinoMarket.tsx
+++ src/components/MatchDetail/CricketCasinoOdds/CricketCasinoMarket.tsx
@@ -135,7 +135,7 @@ const CricketCasinoMarket = ({
                 }}
                 src={ARROWUP}
                 style={{
-                  transform: visible ? "rotate(180deg)" : "rotate(0deg)",
+                  transform: !visible ? "rotate(180deg)" : "rotate(0deg)",
                   width: "15px",
                   height: "15px",
                   marginRight: "5px",
