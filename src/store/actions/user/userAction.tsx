a89cbd74e1262e29500a4e9cddd1998c82262168
Pardeepfairtech <pardeepfairtech@gmail.com>
2025-05-23 16:10:30 +0530

    refactor: update getProfile response handling to access nested data structure

--- src/store/actions/user/userAction.tsx
+++ src/store/actions/user/userAction.tsx
@@ -24,11 +24,11 @@ export const getProfile = createAsyncThunk<any>(
     try {
       const resp = await service.get(ApiConstants.USER.GET_PROFILE);
       if (resp) {
-        if (resp?.data?.loginAt === null) {
+        if (resp?.data[0][0]?.loginAt === null) {
           window.location.replace("/login");
           sessionStorage.clear();
         } else {
-          return resp?.data;
+          return resp?.data[0][0];
         }
       }
     } catch (error: any) {
